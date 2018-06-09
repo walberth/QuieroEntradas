b618a4faf2389f5edef11f7c85b05411c5bd6f0f
Walberth Gutierrez Telles <w.felipe.gutierrez@gmail.com>
2018-06-09 00:50:03 -0500

    add camera support on last search module

--- app/src/main/java/com/cloudvision/utp/quieroentradas/presentation/ui/fragment/LastSearchFragment.java
+++ app/src/main/java/com/cloudvision/utp/quieroentradas/presentation/ui/fragment/LastSearchFragment.java
@@ -1,7 +1,11 @@
 package com.cloudvision.utp.quieroentradas.presentation.ui.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,4 +30,17 @@ public class LastSearchFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_lastsearch, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        FloatingActionButton fabCamera = view.findViewById(R.id.fabCamera);
+        fabCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivity(intent);
+            }
+        });
+    }
 }
